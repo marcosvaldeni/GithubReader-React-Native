ea3fd16f843856ddc7a725c2a54945c27f80e1f4
Marcos Lucas <marcosvaldeni@gmail.com>
2020-06-06 17:40:47 +0100

    Creating project

---
+++ App.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.sectionContainer}>
+        <Text style={styles.sectionTitle}>Hello!</Text>
+        <Text style={styles.sectionDescription}>
+          GitHub Reader version React Native
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
